8557826b4cfa716b3923d34d93cfafeaa635bfd8
Lorenzo Affetti <affox90@gmail.com>
2015-10-02 14:19:05 +0200

    better tuple emission

--- src/main/java/DataGenerator.java
+++ src/main/java/DataGenerator.java
@@ -10,39 +10,54 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 /**
  * Created by affo on 01/10/15.
+ * <p/>
+ * The tuple:
+ * 0 - "medallion", //	an md5sum of the identifier of the taxi - vehicle bound
+ * 1 - "hack_license", // an md5sum of the identifier for the taxi license
+ * 2 - "pickup_datetime", // time when the passenger(s) were picked up
+ * 3 - "dropoff_datetime", // time when the passenger(s) were dropped off
+ * 4 - "trip_time_in_secs", // duration of the trip
+ * 5 - "trip_distance", // trip distance in miles
+ * 6 - "pickup_longitude", // longitude coordinate of the pickup location
+ * 7 - "pickup_latitude", // latitude coordinate of the pickup location
+ * 8 - "dropoff_longitude", // longitude coordinate of the drop-off location
+ * 9 - "dropoff_latitude", // latitude coordinate of the drop-off location
+ * 10 - "payment_type", // the payment method - credit card or cash
+ * 11 - "fare_amount", // fare amount in dollars
+ * 12 - "surcharge", // surcharge in dollars
+ * 13 - "mta_tax", // tax in dollars
+ * 14 - "tip_amount", // tip in dollars
+ * 15 - "tolls_amount", // bridge and tunnel tolls in dollars
+ * 16 - "total_amount" // total paid amount in dollars
  */
 public class DataGenerator extends BaseRichSpout {
     private static final Logger LOG = LoggerFactory.getLogger(DataGenerator.class);
-    public static final int NO_FIELDS = 17;
+    private static final int NO_FIELDS = 17;
     private SpoutOutputCollector collector;
     private static final String dataPath = "data.sample.csv";
     private BufferedReader reader;
+    private DateFormat dateFmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // 2013-01-01 00:02:00
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
         outputFieldsDeclarer.declare(
                 new Fields(
-                        "medallion", //	an md5sum of the identifier of the taxi - vehicle bound
-                        "hack_license", // an md5sum of the identifier for the taxi license
-                        "pickup_datetime", // time when the passenger(s) were picked up
-                        "dropoff_datetime", // time when the passenger(s) were dropped off
-                        "trip_time_in_secs", // duration of the trip
-                        "trip_distance", // trip distance in miles
-                        "pickup_longitude", // longitude coordinate of the pickup location
-                        "pickup_latitude", // latitude coordinate of the pickup location
-                        "dropoff_longitude", // longitude coordinate of the drop-off location
-                        "dropoff_latitude", // latitude coordinate of the drop-off location
-                        "payment_type", // the payment method - credit card or cash
-                        "fare_amount", // fare amount in dollars
-                        "surcharge", // surcharge in dollars
-                        "mta_tax", // tax in dollars
-                        "tip_amount", // tip in dollars
-                        "tolls_amount", // bridge and tunnel tolls in dollars
-                        "total_amount" // total paid amount in dollars
+                        "taxi_id",
+                        "license",
+                        "pickup_ts",
+                        "dropoff_ts",
+                        "pickup_cell",
+                        "dropoff_cell",
+                        "fare",
+                        "tip"
                 ));
     }
 
@@ -65,13 +80,43 @@ public class DataGenerator extends BaseRichSpout {
                 if (tokens.length != NO_FIELDS) {
                     throw new IOException("format error");
                 }
-                this.collector.emit(new Values((Object[]) tokens));
+
+                double puLat = Double.valueOf(tokens[7]);
+                double puLong = Double.valueOf(tokens[6]);
+                double doLat = Double.valueOf(tokens[9]);
+                double doLong = Double.valueOf(tokens[8]);
+                String pickupCell = AreaMapper.getCellID(puLat, puLong);
+                String dropoffCell = AreaMapper.getCellID(doLat, doLong);
+
+                String taxiID = tokens[0];
+                String license = tokens[1];
+                Date pickupTs = dateFmt.parse(tokens[2]);
+                Date dropoffTs = dateFmt.parse(tokens[3]);
+                double fare = Double.valueOf(tokens[11]);
+                double tip = Double.valueOf(tokens[14]);
+
+                this.collector.emit(
+                        new Values(
+                                taxiID,
+                                license,
+                                pickupTs,
+                                dropoffTs,
+                                pickupCell,
+                                dropoffCell,
+                                fare,
+                                tip
+                        )
+                );
             }
             // else {
             // LOG.info(dataPath + ": FEOF");
             // }
         } catch (IOException e) {
             LOG.error("Error in reading nextTuple", e);
+        } catch (AreaMapper.OutOfGridException e) {
+            LOG.info(e.getMessage());
+        } catch (ParseException e) {
+            LOG.error("Error in parsing datetime", e);
         }
     }
 }
