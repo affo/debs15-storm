b44fe7ab0deef11d7c4618ef217b3cc36db97710
Lorenzo Affetti <lorenzo.affetti@gmail.com>
2015-12-03 18:46:41 +0100

    [windowBolt] make windowBolt ACK tuples

--- src/main/java/storm/WindowBolt.java
+++ src/main/java/storm/WindowBolt.java
@@ -1,18 +1,29 @@
 package storm;
 
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by affo on 02/10/15.
  */
 public abstract class WindowBolt extends BaseRichBolt {
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(WindowBolt.class);
     private List<Tuple> window = new ArrayList<>();
     private int windowSize;
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.collector  = outputCollector;
+    }
 
     /**
      * @param windowSize size of the window in seconds
@@ -38,8 +49,10 @@ public abstract class WindowBolt extends BaseRichBolt {
         for (int i = 0; i < removeStop; i++) {
             window.remove(i);
         }
+
         window.add(tuple);
         onWindow(this.window);
+        collector.ack(tuple);
     }
 
     public abstract void onWindow(List<Tuple> window);
